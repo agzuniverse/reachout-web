87bea8519b8f185b0eb504587d41f77a9318ac35
123vivekr <123vivekr@gmail.com>
2018-08-04 19:48:48 +0530

    request page complete

--- src/components/Request.js
+++ src/components/Request.js
@@ -6,64 +6,113 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import {List, ListItem} from 'material-ui/List';
 import ActionInfo from 'material-ui/svg-icons/action/info';
 import FlatButton from 'material-ui/FlatButton';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+import '../css/Request.css';
 
 class Request extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            details:[{
-                name: 'Aswin',
-                location: 'Vytilla',
-                requesting: [
-                    'Food',
-                    'Water',
-                ],
-            },            
-            {
-                name:'Vivek',
-                location: 'SN',
-                requesting: [
-                    'Food',
-                    'water',
-                ],
-            }
+            data: [
+                {
+                    details: {
+                        user:{
+                            name:'John Doe',
+                            bloodgroup:'A+',
+                            phone:'8089967299',
+                        },
+                        location: {
+                            latitude:'80.8999',
+                            longitude:'80.6767',
+                        },
+                        resources: {
+                            food:true,
+                            water:true,
+                            shelter:true,
+                            first_aid:true,
+                            blankets:true,
+                            clothes:true,
+                            medical:true,
+                            transport:true,
+                            ppl:4,
+                            desc:'Help me please',
+                        }
+                    }
+                },
+                {
+                    details: {
+                        user:{
+                            name:'John Doe',
+                            bloodgroup:'A+',
+                            phone:'8089967299',
+                        },
+                        location: {
+                            latitude:'80.8999',
+                            longitude:'80.6767',
+                        },
+                        resources: {
+                            food:true,
+                            water:true,
+                            shelter:true,
+                            first_aid:true,
+                            blankets:true,
+                            clothes:true,
+                            medical:true,
+                            transport:true,
+                            ppl:4,
+                            desc:'Help me please',
+                        }
+                    }
+                }
             ],
-        }
+        };
     }
 
     genCard = (details) => (
-        <Card>
-          <CardHeader
-            title={details.name}
-            subtitle={details.location}
-            actAsExpander={true}
-            showExpandableButton={true}
-          />
-          <CardActions>
-            <List>
-            {
-                details.requesting.map((item) => 
-                    (<ListItem primaryText={ item } rightIcon={<ActionInfo />} />)
-                )
-            }   
-            </List>
-          </CardActions>
-          <CardText expendable={true}>
-          </CardText>
-        </Card>
+        <div className='Card'>
+            <Card>
+              <CardHeader
+                title={details.user.name}
+                subtitle={details.user.bloodgroup + ';' + details.user.phone}
+                actAsExpander={true}
+                showExpandableButton={true}
+              />
+              <CardActions>
+                <List>
+                {
+                    Object.keys(details.resources).map(item => {
+                            if(item != 'desc' && item != 'ppl') {
+                                var bool = details.resources[item];
+                                console.log(details.resources[item]);
+                                if(bool === true)
+                                    // return (<ListItem primaryText={item} rightIcon={<ActionInfo />} />)
+                                    return(<FlatButton label={item} />)
+                            }
+                        }
+                    )
+                }   
+                </List>
+              </CardActions>
+              <CardText expendable={true}>
+              </CardText>
+            </Card>
+            <br />
+        </div>
     );
 
 
     render() {
+        this.state.data.forEach(item => console.log(item.details));
         return(
         <MuiThemeProvider>
             <div className="Request">
               <AppBar
-                title={<span >Request</span>}
+                title={<span>Request</span>}
               />
-                { this.state.details.map((details) => this.genCard(details)) }
+              <div className='Items'>
+                { this.state.data.map(object => this.genCard(object.details)) }
+            </div>
             </div>
         </MuiThemeProvider>
         );
