7f4fa299ced848d5ba0812c24f1d260ceb30b1b6
123vivekr <123vivekr@gmail.com>
2018-08-04 15:32:10 +0530

    routes done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Provider }  from 'react-redux';
+
+import Help from './components/Help';
+import Request from './components/Request';
+import Statistics from './components/Statistics';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      //  <Provider store={ store }>
+         <Router>
+           <div>
+              <Route exact path='/help' component={Help} />
+              <Route exact path='/request' component={Request} />
+              <Route exact path='/statistics' component={Statistics} />
+              {/* <Path exact path='/' */}
+            </div>
+         </Router>
+      //  </Provider>
     );
   }
 }
