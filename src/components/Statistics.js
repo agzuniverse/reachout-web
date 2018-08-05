822f5e996536022e1676170048509f0972cde55f
123vivekr <123vivekr@gmail.com>
2018-08-05 10:30:31 +0530

    charts done

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -4,14 +4,19 @@ import Toolbar from "material-ui/Toolbar";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Card, CardActions, CardHeader, CardText } from "material-ui/Card";
 import FlatButton from "material-ui/FlatButton";
-import { Chart, Axis, Series, Tooltip, Cursor, Line } from "react-charts";
+import Paper from 'material-ui/Paper';
+import { Bar } from 'react-charts';
+import { ColumnChart, BarChart } from 'react-chartkick';
+
+import Chart from 'chart.js'
 
 class Statistics extends Component {
   constructor(props) {
     super(props);
     this.state = {
       request: [],
-      help: []
+      help: [],
+      processData: {},
     };
   }
 
@@ -40,6 +45,7 @@ class Statistics extends Component {
             mainComp.setState({
               help: help
             });
+            mainComp.processData();
             console.log(help);
           }
         };
@@ -51,30 +57,101 @@ class Statistics extends Component {
     xhttp.send();
   }
 
+  processData = () => {
+    let house_ask = 0;  
+    let house_provide = 0;
+    let food_ask = 0;
+    let food_provide = 0;
+    let blood_ask = 0;
+    let blood_provide = 0;
+    if(this.state.help.data) {
+      this.state.help.data.map(object => {
+        if(object.resources.shelter === "true")
+          house_ask = house_ask + 1;
+        if(object.resources.food == "true")
+          food_ask = food_ask + 1;
+        if(object.resources.blood == "true")
+          blood_ask = blood_ask + 1;
+      }) 
+    }
+    if(this.state.request) {
+      this.state.request.map(object => {
+        console.log(object);
+        if(object.resources.shelter == "true")
+          house_provide = house_provide + 1;
+        if(object.resources.food == "true")
+          food_provide = food_provide + 1;
+        if(object.resources.blood == "true")
+          blood_provide = blood_provide + 1;
+      }) 
+    }
+    this.setState({
+      processData: {
+        house_ask: house_ask,
+        house_provide: house_provide,
+        food_ask: food_ask,
+        food_provide: food_provide,
+        blood_ask: blood_ask,
+        blood_provide: blood_provide,
+        needHelp: this.state.help.data.length,
+        receiveHelp: this.state.request.length,
+      }
+    });
+  }
+
   render() {
     console.log(this.state);
+    const style = {
+      // height: 100,
+      // width: 100,
+      margin: 30,
+      textAlign: 'center',
+      display: 'inline-block',
+    };  
     return (
       <MuiThemeProvider>
         <div className="Statistics">
-          <AppBar title={<span>Statistics</span>} />
-          <div style={{ height: "30px" }}>
-            {/* <Chart
-    data={[
-      {
-        label: "Series 1",
-        data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
-      },
-      {
-        label: "Series 2",
-        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
-      }
-    ]}
-  >
-    <Axis primary type="time" />
-    <Axis type="linear" />
-    <Series type={Line} />
-  </Chart> */}
+          <AppBar title={<span>Statistics</span>}/>
+            <ColumnChart style={{marginLeft: '30px'}} width="800px" data={[["Need Help", this.state.processData.needHelp], ["Receive Help", this.state.processData.receiveHelp]]} />
+          <div className='row'> 
+            <span>
+            <Paper style={style} zDepth={1}>
+              <h3>&nbsp; No. of people asking for housing: {this.state.processData.house_ask}&nbsp;&nbsp;</h3>   
+            </Paper>
+            </span>
+            <span style={{paddingLeft: '150px'}}>
+              <Paper style={style} zDepth={1}>
+                <h3>&nbsp; No. of people willing to provide housing: {this.state.processData.house_provide}&nbsp;&nbsp;</h3>   
+              </Paper>
+            </span>
           </div>
+          <BarChart width="800px" data={[["Need Housing", this.state.processData.house_ask], ["Provide Housing", this.state.processData.house_provide]]} />
+          <div className='row2'>
+            <span>
+              <Paper style={style} zDepth={1}>
+                <h3>&nbsp; No. of people asking for blood: {this.state.processData.blood_ask}&nbsp;&nbsp;</h3>   
+              </Paper>
+            </span>
+            <span style={{paddingLeft: '150px'}}>
+              <Paper style={style} zDepth={1}>
+                <h3>&nbsp; No. of people willing to provide blood: {this.state.processData.blood_provide}&nbsp;&nbsp;</h3>   
+              </Paper>
+            </span>
+          </div>
+          <BarChart width="800px" data={[["Need Blood", this.state.processData.blood_ask], ["Provide Blood", this.state.processData.blood_provide]]} />
+          <div className='row3'>
+             <span>
+              <Paper style={style} zDepth={1}>
+                <h3>&nbsp; No. of people asking for food: {this.state.processData.food_ask}&nbsp;&nbsp;</h3>   
+              </Paper>
+            </span>
+            <span style={{paddingLeft: '150px'}}>
+              <Paper style={style} zDepth={1}>
+                <h3>&nbsp; No. of people willing to provide food: {this.state.processData.food_provide}&nbsp;&nbsp;</h3>   
+              </Paper>
+            </span>        
+          </div>
+          <BarChart width="800px" data={[["Need Food", this.state.processData.food_ask], ["Provide Food", this.state.processData.food_provide]]} />
         </div>
       </MuiThemeProvider>
     );
