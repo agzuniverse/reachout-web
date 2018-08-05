0d2b7c433534dc59a737cb95ba1f0d43665bb230
Aswin G <aswinganesh666@gmail.com>
2018-08-05 08:34:44 +0530

    Fix XHR calls

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,68 +1,64 @@
-import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
+import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import { Card, CardActions, CardHeader, CardText } from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
 import { Chart, Axis, Series, Tooltip, Cursor, Line } from "react-charts";
 
-
 class Statistics extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          request: [],
-          help: [], 
-        };
-    }
-
-    componentWillMount() {
-      let request = [], help = [];
-      let mainComp = this;
-      let URL1 = "https://8f555758.ngrok.io/api/getalloffers";
-      let URL2 = "https://8f555758.ngrok.io/api/getallreqs";
-      console.log("df");
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                console.log(xhttp.responseText);
-               request = JSON.parse(xhttp.responseText);
-               console.log(request);
-               request = request.data;
-              request.shift();
-              mainComp.setState({
-                request: request,
-              });
-                var xhttp2 = new XMLHttpRequest();
-                xhttp2.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                      // console.log(xhttp.responseText)
-                      help = JSON.parse(xhttp2.responseText);
-                      mainComp.setState({
-                        help: help,
-                      });      
-                      console.log(help);
-                    }
-                xhttp2.open("GET", URL2, true);
-                xhttp2.send();       
-                  }
-              };
-              xhttp.open("GET", URL1, true);
-              xhttp.send();
-          }
+  constructor(props) {
+    super(props);
+    this.state = {
+      request: [],
+      help: []
+    };
   }
 
-    render() {
-      console.log(this.state);
-        return(
-        <MuiThemeProvider>
-            <div className="Statistics">
-              <AppBar
-                title={<span>Statistics</span>}
-              />
-              <div style={{height: '30px'}}>
-                {/* <Chart
+  componentWillMount() {
+    let request = [],
+      help = [];
+    let mainComp = this;
+    let URL1 = "https://8f555758.ngrok.io/api/getalloffers";
+    let URL2 = "https://8f555758.ngrok.io/api/getallreqs";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(xhttp.responseText);
+         request = JSON.parse(xhttp.responseText);
+        console.log(request);
+        request = request.data;
+        request.shift();
+        mainComp.setState({
+          request: request
+        });
+        var xhttp2 = new XMLHttpRequest();
+        xhttp2.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            // console.log(xhttp.responseText)
+            help = JSON.parse(xhttp2.responseText);
+            mainComp.setState({
+              help: help
+            });
+            console.log(help);
+          }
+        };
+        xhttp2.open("GET", URL2, true);
+        xhttp2.send();
+      }
+    };
+    xhttp.open("GET", URL1, true);
+    xhttp.send();
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <MuiThemeProvider>
+        <div className="Statistics">
+          <AppBar title={<span>Statistics</span>} />
+          <div style={{ height: "30px" }}>
+            {/* <Chart
     data={[
       {
         label: "Series 1",
@@ -78,11 +74,11 @@ class Statistics extends Component {
     <Axis type="linear" />
     <Series type={Line} />
   </Chart> */}
-  </div>
-            </div>
-        </MuiThemeProvider>
-        );
-    }
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default Statistics;
